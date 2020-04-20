9cd34705eb1918e84b7ddc963ce21adba0d91d04
Sidian.Luo <Sidian.Luo@geely.com>
2020-04-20 21:14:05 +0800

    feature develop: 表格布局字段--20%

--- src/components/filed/GTable.vue
+++ src/components/filed/GTable.vue
@@ -1,21 +1,79 @@
 <template>
     <div class="table">
-        <table>
-
+        <table class="inner-table">
+            <colgroup>
+                <col :style="`width:${(1/field.column)*100}%`"
+                     v-for="(item,index) in columnArrayForIterate"
+                     :key="index"
+                />
+            </colgroup>
+            <tbody>
+                <tr v-for="(row,index) in field.cells"
+                    :key="index"
+                    class="row"
+                >
+                    <td v-for="(cell,index) in row"
+                        :key="index"
+                        class="cell"
+                        :colspan="cell.width"
+                        :rowspan="cell.height"
+                    >
+                    </td>
+                </tr>
+            </tbody>
         </table>
     </div>
 </template>
 
 <script>
+    import utils from "../../assets/utils";
+
     export default {
         name: "GTable",
         props:['field'],
+        computed:{
+            /**
+             * 仅用于遍历的数据
+             * @return {[]}
+             */
+            columnArrayForIterate(){
+                return utils.fillArray(this.field.column,1);
+            }
+        },
         created() {
             //初始化所有单元格
+            this.init();
+        },
+        methods:{
+            init(){
+                let cells=[];
+                for(let i=0;i<this.field.row;i++){
+                    let rowCells=[];
+                    for(let j=0;j<this.field.column;j++){
+                        rowCells.push({
+                            row:i,column:j,width:1,height:1
+                        })
+                    }
+                    cells.push(rowCells);
+                }
+                this.field.cells=cells;
+            }
         }
     }
 </script>
 
 <style lang="scss" scoped>
-
+    .table{
+        width: 100%;
+        .inner-table{
+            width: 100%;
+            border-collapse: collapse;
+            .row{
+                .cell{
+                    height: 2rem;
+                    border:solid 1px black;
+                }
+            }
+        }
+    }
 </style>
